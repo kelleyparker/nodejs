868e708661b670dc0d4a1450bf7d04ca439c95c4
Kelley Parker <kelley.parker07@gmail.com>
2022-04-03 16:59:01 -0700

    add

--- first.js
+++ first.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-
 var http = require('http');
+var dt = require('./myfirstmodule');
+
 
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
